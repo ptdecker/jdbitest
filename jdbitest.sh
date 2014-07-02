0d44063954c1dc456566e14e65f86e10dca8247e
ptdecker <ptdecker@mac.com>
2014-07-02 14:16:23 -0500

    Initial commit

---
+++ jdbitest.sh
@@ -0,0 +1,4 @@
+#A simple bash shell script to run LigoMCP
+#!/bin/bash
+
+java -cp target/JDBITest-1.0-SNAPSHOT.jar org.ptodd.jdbitest.JDBITest
---
+++ src/main/java/org/ptodd/jdbitest/JDBITest.java
@@ -0,0 +1,12 @@
+package org.ptodd.jdbitest;
+
+/**
+ * Hello world!
+ *
+ */
+public class JDBITest {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
+

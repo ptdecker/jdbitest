071d33d7e35748751e777cb4d2e6f54571f12019
ptdecker <ptdecker@mac.com>
2014-07-02 15:01:28 -0500

    Working code using JDBC driver (instead of JDBI)

--- jdbitest.sh
+++ jdbitest.sh
@@ -1,4 +1,4 @@
 #A simple bash shell script to run LigoMCP
 #!/bin/bash
 
-java -cp target/JDBITest-1.0-SNAPSHOT.jar org.ptodd.jdbitest.JDBITest
+java -cp target/jdbitest-1.0-SNAPSHOT.jar org.ptodd.jdbitest.JDBITest
--- src/main/java/org/ptodd/jdbitest/JDBITest.java
+++ src/main/java/org/ptodd/jdbitest/JDBITest.java
@@ -1,12 +1,54 @@
 package org.ptodd.jdbitest;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class JDBITest {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+    public static void main(String[] args) {
+
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:postgresql://localhost/ptdecker";
+        String user = "postgres";
+        String password = "Duckey21";
+
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT VERSION()");
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(JDBITest.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (st != null) {
+                    st.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(JDBITest.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
     }
 }
-
